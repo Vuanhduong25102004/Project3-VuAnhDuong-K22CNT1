2390c02dade9da3d14a4ab015185600a095c731b
Vuanhduong25102004 <vuanhduong251020042@gmail.com>
2025-03-18 08:19:55 +0700

    Project3

--- QL_Lich_Kham_Vu_Anh_Duong_K22CNT1/src/main/java/com/repository/VadAppointmentsRepository.java
+++ QL_Lich_Kham_Vu_Anh_Duong_K22CNT1/src/main/java/com/repository/VadAppointmentsRepository.java
@@ -3,10 +3,20 @@ package com.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.model.VadAppointments;
 import com.model.VadUser;
 
 public interface VadAppointmentsRepository extends JpaRepository<VadAppointments, Long> {
-	List<VadAppointments> findByUser(VadUser user);
+	@Query("SELECT a FROM VadAppointments a " +
+	           "WHERE a.user = :user " +
+	           "ORDER BY CASE " +
+	           "           WHEN a.vadStatus = 'pending' THEN 1 " +
+	           "           WHEN a.vadStatus = 'approved' THEN 2 " +
+	           "           WHEN a.vadStatus = 'canceled' THEN 3 " +
+	           "           WHEN a.vadStatus = 'completed' THEN 4 " +
+	           "         END ASC")
+	List<VadAppointments> findByUser(@Param("user") VadUser user);
 }
